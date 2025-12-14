67975e15160a23e06e5186885f899071f52f3871
unknown <jayyagami9@gmail.com>
2025-12-14 08:22:15 +0530

    demo commit

--- src/main/java/com/workingspringboot/projectDecember/ProjectDecemberApplication.java
+++ src/main/java/com/workingspringboot/projectDecember/ProjectDecemberApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ProjectDecemberApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(ProjectDecemberApplication.class, args);
+		
 	}
 
 }
