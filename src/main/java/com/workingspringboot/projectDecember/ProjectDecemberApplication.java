418669eb7fa80cde10b2fd994e1a8b0805d6ed7f
unknown <jayyagami9@gmail.com>
2025-12-14 07:48:21 +0530

    creating a new spring boot project

---
+++ src/main/java/com/workingspringboot/projectDecember/ProjectDecemberApplication.java
@@ -0,0 +1,13 @@
+package com.workingspringboot.projectDecember;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectDecemberApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectDecemberApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/workingspringboot/projectDecember/ProjectDecemberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.workingspringboot.projectDecember;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectDecemberApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
